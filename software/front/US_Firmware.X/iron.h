a0733b0ccfaeb6717f98708cbc29849bc1a8adc7
Pascal Brunot <pascal.brunot@gmail.com>
2024-06-15 14:40:48 +0200

    New menu option "MODEL"
    AUTO -> keep autodetection
    Fixed model otherwise

--- software/front/US_Firmware.X/iron.h
+++ software/front/US_Firmware.X/iron.h
@@ -13,50 +13,54 @@ extern "C" {
 #endif
 #include <GenericTypeDefs.h>
 #include "typedefs.h"
-    
-typedef struct __PACKED {
+
+    typedef struct __PACKED {
+
         struct __PACKED {
-            UINT8 Type;             //sensor type (1: TC, 2: PTC)
-            unsigned int HChannel :1; //Heater Channel
-            unsigned int InputP :1;    //positive input sensor channel selection (0=GND; 1=sensor channel B)
-            unsigned int InputN :1;    //negative input sensor channel selection (0=GND; 1=sensor channel A)
-            unsigned int InputInv :1;  //input amplifier invert (0=not inverted (ChP on +, ChN on -), 1=inverted(ChN on +, ChP on -)
-            unsigned int CBandA :1; //sensor channel A current source band (0=x16, 1=x1)
-            unsigned int CBandB :1; //sensor channel B current source band (0=x16, 1=x1)
-            unsigned int Reserved :18;
+            UINT8 Type; //sensor type (1: TC, 2: PTC)
+            unsigned int HChannel : 1; //Heater Channel
+            unsigned int InputP : 1; //positive input sensor channel selection (0=GND; 1=sensor channel B)
+            unsigned int InputN : 1; //negative input sensor channel selection (0=GND; 1=sensor channel A)
+            unsigned int InputInv : 1; //input amplifier invert (0=not inverted (ChP on +, ChN on -), 1=inverted(ChN on +, ChP on -)
+            unsigned int CBandA : 1; //sensor channel A current source band (0=x16, 1=x1)
+            unsigned int CBandB : 1; //sensor channel B current source band (0=x16, 1=x1)
+            unsigned int Reserved : 18;
         };
-    UINT16  CurrentA;       //current for A sensor channel (0-256) current = CurrentA * (1.225/(1.6*256))mA
-    UINT16  CurrentB;       //current for B sensor channel
-    UINT16  Gain;           //Amplifier gain for sensor, Gain = x*4, x=8-256
-    UINT16  Offset;         //offset to exclude from measurement (0-1024)
-    float   TPoly[10];      //Temperature polynomial coefficients    
-}t_SensorConfig;    
+        UINT16 CurrentA; //current for A sensor channel (0-256) current = CurrentA * (1.225/(1.6*256))mA
+        UINT16 CurrentB; //current for B sensor channel
+        UINT16 Gain; //Amplifier gain for sensor, Gain = x*4, x=8-256
+        UINT16 Offset; //offset to exclude from measurement (0-1024)
+        float TPoly[10]; //Temperature polynomial coefficients    
+    } t_SensorConfig;
 
-typedef struct __PACKED {
-    t_SensorConfig SensorConfig;
-    INT16   HRCompCurrent;  //heater resistance compensating current (0, currentA, currentB or currentA+currentB)
-    INT8    WSLen;          //Waveshaping, 0=off, 1-8 = sample waveshaping with length 1-8, -1 = damping waveshaping
-    UINT8   PID_DGain;      //PID differential gain (0 - 32)
-    UINT16  PID_KP;         //PID proportional coefficient (0.1 - 0.999) * 32768
-    UINT16  PID_KI;         //PID integral coefficient (0.1 - 0.999) * 32768
-    UINT16  PID_OVSGain;    //Positive overshoot prevention gain (0 - 32)
-    UINT16  PID_PMax;       //maximum rated power (watts)
-    UINT16  PID_PNom;       //nominal power for which PID coefficients are calculated
-} t_IronConfig;
+    typedef struct __PACKED {
+        t_SensorConfig SensorConfig;
+        INT16 HRCompCurrent; //heater resistance compensating current (0, currentA, currentB or currentA+currentB)
+        INT8 WSLen; //Waveshaping, 0=off, 1-8 = sample waveshaping with length 1-8, -1 = damping waveshaping
+        UINT8 PID_DGain; //PID differential gain (0 - 32)
+        UINT16 PID_KP; //PID proportional coefficient (0.1 - 0.999) * 32768
+        UINT16 PID_KI; //PID integral coefficient (0.1 - 0.999) * 32768
+        UINT16 PID_OVSGain; //Positive overshoot prevention gain (0 - 32)
+        UINT16 PID_PMax; //maximum rated power (watts)
+        UINT16 PID_PNom; //nominal power for which PID coefficients are calculated
+    } t_IronConfig;
 
-typedef struct __PACKED {
-    UINT16 Version;
-    UINT16_VAL ID;
-    char Name[24];    
-    t_IronConfig Config[2];
-    const t_SensorConfig * ColdJunctionSensorConfig;
-} t_IronPars;
+    typedef struct __PACKED {
+        UINT16 Version;
+        UINT16_VAL ID;
+        char Name[24];
+        t_IronConfig Config[2];
+        const t_SensorConfig * ColdJunctionSensorConfig;
+    } t_IronPars;
 
-/* Iron temperature sensor types definition */
+    /* Iron temperature sensor types definition */
 #define SENSOR_UNDEFINED  0
 #define SENSOR_TC         1
 #define SENSOR_PTC        2
 #define SENSOR_NONE     255
+
+    // Total number of configured irons
+#define NB_IRONS 13
 
 #ifndef _IRON_C
 #define IRON_H_EXTERN extern
@@ -64,10 +68,13 @@ typedef struct __PACKED {
 #define IRON_H_EXTERN
 #endif
 
-IRON_H_EXTERN UINT16 IronID;
-IRON_H_EXTERN volatile t_IronPars IronPars;
-IRON_H_EXTERN void IronInit();
-IRON_H_EXTERN void IronTasks();
+    IRON_H_EXTERN UINT16 IronID;
+    IRON_H_EXTERN volatile t_IronPars IronPars;
+    IRON_H_EXTERN void IronInit();
+    IRON_H_EXTERN void IronTasks();
+
+    // Description of a given iron by its index 
+    IRON_H_EXTERN const char* IronDesc(UINT8 index);
 
 #undef IRON_H_EXTERN
 
